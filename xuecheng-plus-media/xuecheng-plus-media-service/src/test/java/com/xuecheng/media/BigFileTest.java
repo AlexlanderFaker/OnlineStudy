848ceda7bf2c88a5541aef8130b5e008c72d281e
唐小飞 <16563056695@qq.com>
2023-05-04 23:53:36 +0800

    文件分段上传下载测试类

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/BigFileTest.java
@@ -0,0 +1,20 @@
+package com.xuecheng.media;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Mr.Tang
+ * @version 1.0
+ * @description
+ * @date ${LocalDateTime.now()}
+ */
+public class BigFileTest {
+    @Test
+    public void testChunk(){
+
+    }
+    @Test
+    public void testMerge(){
+        
+    }
+}
